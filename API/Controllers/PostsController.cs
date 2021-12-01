e1e5587a0f79c2d9ac2570f9849a5d6bde5d8e50
WinslowJosiah <winslowjosiah@gmail.com>
2021-11-30 20:34:24 -0600

    Completed Activity 6

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -6,6 +6,7 @@ using Application.Posts;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Persistence;
 
 namespace API.Controllers
 {
@@ -14,14 +15,86 @@ namespace API.Controllers
 
     public class PostsController : ControllerBase
     {
-        private readonly IMediator mediator;
+        private readonly DataContext context;
 
-        public PostsController(IMediator mediator) => this.mediator = mediator;
+        public PostsController(DataContext context) {
+            this.context = context;
+        }
 
+        /// <summary>
+        /// GET /posts
+        /// </summary>
+        /// <returns>A list of posts</returns>
         [HttpGet]
-        public async Task<ActionResult<List<Post>>> List()
+        public ActionResult<List<Post>> Get()
         {
-            return await this.mediator.Send(new List.Query());
+            return this.context.Posts.ToList();
+        }
+
+        /// <summary>
+        /// GET /post/id
+        /// </summary>
+        /// <param name="id">Post id</param>
+        /// <returns>A single post</returns>
+        [HttpGet("{id}")]
+        public ActionResult<Post> GetById(Guid id)
+        {
+            return this.context.Posts.Find(id);
+        }
+
+        /// <summary>
+        /// POST /post
+        /// </summary>
+        /// <param name="request">JSON request containing post fields</param>
+        /// <returns>A new post</returns>
+        [HttpPost]
+        public ActionResult<Post> Create([FromBody] Post request) {
+            var post = new Post
+            {
+                Id = request.Id,
+                Title = request.Title,
+                Body = request.Body,
+                Date = request.Date
+            };
+
+            context.Posts.Add(post);
+            var success = context.SaveChanges() > 0;
+
+            if (success)
+            {
+                return post;
+            }
+
+            throw new Exception("Error creating post");
+        }
+
+        /// <summary>
+        /// PUT /put
+        /// </summary>
+        /// <param name="request">JSON request containing one or more updated post fields</param>
+        /// <returns>An updated post</returns>
+        [HttpPut]
+        public ActionResult<Post> Update([FromBody] Post request) {
+            var post = context.Posts.Find(request.Id);
+
+            if (post == null)
+            {
+                throw new Exception("Could not find post");
+            }
+
+            // Update the post properties with request values, if present
+            post.Title = request.Title != null ? request.Title : post.Title;
+            post.Body = request.Body != null ? request.Body : post.Body;
+            post.Date = request.Date != null ? request.Date : post.Date;
+
+            var success = context.SaveChanges() > 0;
+
+            if (success)
+            {
+                return post;
+            }
+
+            throw new Exception("Error updating post");
         }
     }
 }
