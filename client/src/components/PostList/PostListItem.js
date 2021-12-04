e9042250e31eadecf24f919b683575a696427899
Josiah Winslow <winslowjosiah@gmail.com>
2021-12-03 22:26:13 -0600

    Completed Activity 8

---
+++ client/src/components/PostList/PostListItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import slugify from "slugify";
+import "./styles.css";
+
+const PostListItem = props => {
+    const { post, clickPost } = props;
+    const navigate = useNavigate();
+
+    const handleClickPost = post => {
+        const slug = slugify(post.title, { lower: true });
+
+        clickPost(post);
+        navigate(`/posts/${slug}`);
+    };
+
+    return (
+        <div>
+            <div className="postListItem" onClick={() => handleClickPost(post)}>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+            </div>
+        </div>
+    );
+};
+
+export default PostListItem;
